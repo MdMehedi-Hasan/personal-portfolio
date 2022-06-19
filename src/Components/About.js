17cf1b04e1d40b2b6ce6d64175da733d2cabf997
MdMehedi-Hasan <mh19115@gmail.com>
2022-06-19 16:37:38 +0600

    about component responsive complete

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,19 +4,21 @@ import image from '../images/wave.png'
 import profile from '../images/profile.png'
 import { Link } from 'react-router-dom';
 import './About.css'
+import Footer from './Footer';
+import ProjectCards from './ProjectCards';
 
 const About = () => {
     return (
-        <div>
+        <div className='text-white'>
             <section className=''>
                 <img className='' src={image} alt="" />
                 {/* <h1 className='text-4xl text-white text-center'>Hey mate! Welcome to my portfolio.</h1>
                 <p className='text-xl text-white text-center'>Learn more about me</p> */}
             </section>
-            <section className='text-white grid gap-16 grid-cols-1fr-2fr mx-10 mt-[-180px]'>
-                <aside className=' py-10 h-fit border-2 rounded-2xl mt-[-250px] bg-cyan-900 border-cyan-900'>
+            <section className='text-white grid gap-16 grid-cols-1fr-2fr lg:mx-10 mx-2 lg:mt-[-180px]'>
+                <aside className=' py-10 h-fit border-2 rounded-2xl lg:mt-[-250px] bg-cyan-900 border-cyan-900'>
                     <div className="avatar flex justify-center">
-                        <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                        <div className="w-24 rounded-full ring ring-accent ring-offset-base-100 ring-offset-2">
                             <img src={profile} alt="" />
                         </div>
                     </div>
@@ -31,70 +33,97 @@ const About = () => {
                     <div className=' w-11/12 mx-auto pb-10 mb-10 border-b-2 border-dotted'>
                         <div className='flex items-center mb-2 justify-between'><span className='font-bold font-mono'>Country:</span><span>Bangladesh</span></div>
                         <div className='flex items-center mb-2 justify-between'><span className='font-bold font-mono'>City:</span><span>Dhaka</span></div>
-                        <div className='flex items-center mb-2 justify-between'><span className='font-bold font-mono'>Email:</span><span className='text-sm'>mehedihasan.webdev@gmail.com</span></div>
+                        <div className='flex items-center mb-2 justify-between'><span className='font-bold font-mono'>Email:</span><small className='text-right'>mehedihasan. webdev @ gmail. com</small></div>
                         <div className='flex items-center justify-between'><span className='font-bold font-mono'>Phone:</span><span>+8801320676252</span></div>
                     </div>
                     <Link to="/contact" className='flex justify-center mt-5 text-lg'><button className='flex items-center border px-4 py-2 rounded-full'><span className='mr-2'>Contact me</span> <Icon icon="eva:email-fill" /></button></Link>
                 </aside>
                 <div>
-                    <div className='grid grid-cols-3 gap-5 h-36 text-2xl'>
-                        <div className='border border-cyan-700 shadow-2xl flex flex-col pl-5 justify-center rounded-xl bg-cyan-900'><div className='flex items-center text-4xl'><Icon icon="ic:twotone-developer-mode" /><span>120+</span></div> <span className='mt-5'>Total projects</span></div>
-                        <div className='border border-cyan-700 shadow-2xl flex flex-col pl-5 justify-center rounded-xl bg-cyan-900'><div className='flex items-center text-4xl'><Icon icon="ic:twotone-developer-mode" /><span>120+</span></div><span className='mt-5'>Full stack projects</span></div>
-                        <div className='border border-cyan-700 shadow-2xl flex flex-col pl-5 justify-center rounded-xl bg-cyan-900'><div className='flex items-center text-4xl'><Icon icon="ic:twotone-developer-mode" /><span>120+</span></div><span className='mt-5'>Full stack projects</span></div>
+                    <div className='grid lg:grid-cols-3 text-center lg:text-left gap-5 h-36'>
+                        <div className='border border-cyan-700 shadow-2xl flex flex-col lg:pl-5 justify-center rounded-xl bg-cyan-900'><div className='flex items-center justify-center lg:text-4xl'><Icon icon="ic:twotone-developer-mode" /><span>120+</span></div> <span className='mt-5'>Total projects</span></div>
+                        <div className='border border-cyan-700 shadow-2xl flex flex-col lg:pl-5 justify-center rounded-xl bg-cyan-900'><div className='flex items-center justify-center lg:text-4xl'><Icon icon="ic:twotone-developer-mode" /><span>120+</span></div><span className='mt-5'>Full stack projects</span></div>
+                        <div className='border border-cyan-700 shadow-2xl flex flex-col lg:pl-5 justify-center rounded-xl bg-cyan-900'><div className='flex items-center justify-center lg:text-4xl'><Icon icon="ic:twotone-developer-mode" /><span>120+</span></div><span className='mt-5'>Full stack projects</span></div>
                     </div>
-                    <div className="mt-24">
+                    <div className="mt-40">
                         <div className='flex items-center'>
                             <h1 className='text-4xl mr-4'>Skills</h1>
                             <div className='w-11/12 h-4 border-b-2 border-cyan-900 border-dotted'></div>
                         </div>
-                    <div className='flex flex-col items-end mt-10'>
-                        <div className='text-left w-10/12'>
-                        <h1 className='text-left'>HTML</h1>
-                        <progress class="progress progress-accent w-full mb-5" value="95" max="100"></progress>
-                        </div>
-                        <div className='text-left w-10/12'>
-                        <h1 className='text-left'>CSS</h1>
-                        <progress class="progress progress-accent w-full mb-5" value="90" max="100"></progress>
-                        </div>
-                        <div className='text-left w-10/12'>
-                        <h1 className='text-left'>JavaScript</h1>
-                        <progress class="progress progress-accent w-full mb-5" value="80" max="100"></progress>
-                        </div>
-                        <div className='text-left w-10/12'>
-                        <h1 className='text-left'>React</h1>
-                        <progress class="progress progress-accent w-full mb-5" value="85" max="100"></progress>
-                        </div>
-                        <div className='text-left w-10/12'>
-                        <h1 className='text-left'>NodeJs</h1>
-                        <progress class="progress progress-accent w-full mb-5" value="70" max="100"></progress>
-                        </div>
-                        <div className='text-left w-10/12'>
-                        <h1 className='text-left'>Express.js</h1>
-                        <progress class="progress progress-accent w-full mb-5" value="80" max="100"></progress>
-                        </div>
-                        <div className='text-left w-10/12'>
-                        <h1 className='text-left'>MongoDB</h1>
-                        <progress class="progress progress-accent w-full mb-5" value="75" max="100"></progress>
-                        </div>
-                        <div className='text-left w-10/12'>
-                        <h1 className='text-left'>Bootstrap</h1>
-                        <progress class="progress progress-accent w-full mb-5" value="95" max="100"></progress>
-                        </div>
-                        <div className='text-left w-10/12'>
-                        <h1 className='text-left'>Tailwind css</h1>
-                        <progress class="progress progress-accent w-full mb-5" value="90" max="100"></progress>
-                        </div>
-                        <div className='text-left w-10/12'>
-                        <h1 className='text-left'>DaisyUI</h1>
-                        <progress class="progress progress-accent w-full mb-5" value="95" max="100"></progress>
+                        <div className='flex flex-col lg:items-end items-center mt-10'>
+                            <div className='text-left w-9/12'>
+                                <h1 className='text-left'>HTML</h1>
+                                <progress class="progress progress-accent w-full mb-5" value="95" max="100"></progress>
+                            </div>
+                            <div className='text-left w-9/12'>
+                                <h1 className='text-left'>CSS</h1>
+                                <progress class="progress progress-accent w-full mb-5" value="90" max="100"></progress>
+                            </div>
+                            <div className='text-left w-9/12'>
+                                <h1 className='text-left'>JavaScript</h1>
+                                <progress class="progress progress-accent w-full mb-5" value="80" max="100"></progress>
+                            </div>
+                            <div className='text-left w-9/12'>
+                                <h1 className='text-left'>React</h1>
+                                <progress class="progress progress-accent w-full mb-5" value="85" max="100"></progress>
+                            </div>
+                            <div className='text-left w-9/12'>
+                                <h1 className='text-left'>NodeJs</h1>
+                                <progress class="progress progress-accent w-full mb-5" value="70" max="100"></progress>
+                            </div>
+                            <div className='text-left w-9/12'>
+                                <h1 className='text-left'>Express.js</h1>
+                                <progress class="progress progress-accent w-full mb-5" value="80" max="100"></progress>
+                            </div>
+                            <div className='text-left w-9/12'>
+                                <h1 className='text-left'>MongoDB</h1>
+                                <progress class="progress progress-accent w-full mb-5" value="75" max="100"></progress>
+                            </div>
+                            <div className='text-left w-9/12'>
+                                <h1 className='text-left'>Bootstrap</h1>
+                                <progress class="progress progress-accent w-full mb-5" value="95" max="100"></progress>
+                            </div>
+                            <div className='text-left w-9/12'>
+                                <h1 className='text-left'>Tailwind css</h1>
+                                <progress class="progress progress-accent w-full mb-5" value="90" max="100"></progress>
+                            </div>
+                            <div className='text-left w-9/12'>
+                                <h1 className='text-left'>DaisyUI</h1>
+                                <progress class="progress progress-accent w-full mb-5" value="95" max="100"></progress>
+                            </div>
                         </div>
                     </div>
+                </div>
+            </section>
+            <section className='grid lg:grid-cols-2 gap-14 lg:mx-10 mx-2 mt-28'>
+                <div className='h-fit'>
+                    <div className='flex items-center justify-between'>
+                        <h1 className='text-4xl text-white'>About me</h1>
+                        <div className='w-8/12 h-6 border-b-2 border-cyan-900 border-dotted'></div>
+                    </div>
+                    <p className='mt-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, quia quam, at molestiae fuga debitis repellendus recusandae veritatis, facilis unde accusantium ex sit reprehenderit nostrum quasi perferendis rerum a. Maiores?</p>
+                </div>
+                <div>
+                    <div className='flex items-center justify-between'>
+                        <h1 className='text-4xl text-white'>Other information</h1>
+                        <div className='w-5/12 h-6 border-b-2 border-cyan-900 border-dotted'></div>
+                    </div>
+                    <div className=''>
+                        <div className='flex justify-between items-center mb-5 mt-10'><span>Education:</span><span className='text-right'>Undergraduate, B.B.A honours. (Govt. Titumir College)</span></div>
+                        <div className='flex justify-between items-center mb-5'><span>Communication:</span><span className='text-right'>Bangla(Native), English</span></div>
+                        <div className='flex justify-between items-center mb-5'><span>Nationality:</span><span className='text-right'>Bangladeshi</span></div>
+                        <div className='flex justify-between items-center mb-5'><span>Religion:</span><span className='text-right'>Islam</span></div>
+                        <div className='flex justify-between items-center mb-5'><span>Interests:</span><span className='text-right'>Travelling, Gaming, Exploring</span></div>
                     </div>
                 </div>
             </section>
             <section>
-
+            <div className='flex items-center justify-between mx-2 lg:mx-10 mt-28'>
+                        <h1 className='lg:text-4xl text-xl text-white'>Some of my recent projects</h1>
+                        <div className='lg:w-7/12 w-1/4 h-6 border-b-2 border-cyan-900 border-dotted'></div>
+                    </div>
+                <ProjectCards></ProjectCards>
             </section>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,23 +1,10 @@
-import { Icon } from '@iconify/react';
 import React from 'react';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import image from '../images/wave.png'
 import Footer from './Footer';
 import './Project.css'
+import ProjectCards from './ProjectCards';
 
 const Projects = () => {
-    const [projects, setProjects] = useState([]);
-    useEffect(() => {
-        fetch("data.json")
-            .then(res => res.json())
-            .then(data => setProjects(data))
-    }, [])
-    const navigate = useNavigate();
-    const redirect = (id) => {
-        navigate(`/projects/${id}`)
-    }
     return (
         <div className=''>
             <div className='relative'>
@@ -27,26 +14,12 @@ const Projects = () => {
                     <p className='border w-44 lg:w-48 rounded-full p-2 text-center text-white lg:text-xl text-lg lg:mt-12 mt-6 font-sans'>Most recent works</p>
                 </div>
             </div>
-            <div className='grid lg:grid-cols-2 gap-x-16 lg:mt-[-350px] lg:mx-20 border-b-2 border-dotted border-[#114c67aa] pb-12'>
-                {projects.map(project =>
-                    <div key={project.id} className="card lg:mt-20 mb-20 lg:mb-0 shadow-2xl bg-[#114c67aa] text-white">
-                        <figure><img className='h-56 w-full' src={project.image} alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <div className="card-actions justify-start">
-                                {project.tech.map(badge => <div key={badge} className="badge badge-outline">{badge}</div>)}
-                            </div>
-                            <h2 className="card-title">
-                                {project.title}
-                            </h2>
-                            <p>{project.description.slice(0, 200)} <button className="badge" onClick={() => redirect(project.id)}>See more &nbsp;<Icon className='text-xl' icon="ci:small-long-right" /></button></p>
-                            <div className="card-actions justify-center">
-                                <a href="https://www.google.com/search?q=website+icon&sxsrf=ALiCzsYUNkztV1w0KZMuEq7nGUb2Sw15xQ:1655438723214&source=lnms&tbm=isch&sa=X&ved=2ahUKEwigq63ozbP4AhWZ6nMBHVpxC64Q_AUoAXoECAEQAw&biw=1280&bih=657&dpr=1" rel='noreferrer' target="_blank"><button className="text-2xl border border-accent rounded-full p-2 mt-8 hover:bg-accent mr-2 hover:text-white tooltip" data-tip="Live site"><Icon icon="mdi:web" /></button></a>
-                                <a href="https://www.google.com/search?q=website+icon&sxsrf=ALiCzsYUNkztV1w0KZMuEq7nGUb2Sw15xQ:1655438723214&source=lnms&tbm=isch&sa=X&ved=2ahUKEwigq63ozbP4AhWZ6nMBHVpxC64Q_AUoAXoECAEQAw&biw=1280&bih=657&dpr=1" rel='noreferrer' target="_blank"><button className="text-2xl border border-accent rounded-full p-2 mt-8 hover:bg-accent hover:text-white tooltip" data-tip="Github"><Icon icon="akar-icons:github-fill" /></button></a>
-                            </div>
-                        </div>
-                    </div>)}
+            <div className='px-10'>
+            <ProjectCards></ProjectCards>
             </div>
+            <div className='mx-20'>
             <Footer></Footer>
+            </div>
         </div>
     );
 };
